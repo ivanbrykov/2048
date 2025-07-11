6f504cc1efda3779a776fb3ca090b56f54232323
Ivan Brykov <ivan.ru@gmail.com>
2025-07-11 00:35:12 -0500

    fix: Prevent game move on help button tap

--- src/App.tsx
+++ src/App.tsx
@@ -59,11 +59,14 @@ function App() {
     [handleMove, showHelp],
   );
 
-  const handleTouchStart = useCallback((e: React.TouchEvent) => {
-    if (showHelp) return;
-    setTouchStartX(e.touches[0].clientX);
-    setTouchStartY(e.touches[0].clientY);
-  }, [showHelp]);
+  const handleTouchStart = useCallback(
+    (e: React.TouchEvent) => {
+      if (showHelp) return;
+      setTouchStartX(e.touches[0].clientX);
+      setTouchStartY(e.touches[0].clientY);
+    },
+    [showHelp],
+  );
 
   const handleTouchEnd = useCallback(
     (e: React.TouchEvent) => {
@@ -115,114 +118,116 @@ function App() {
   }, [showHelp]);
 
   return (
-    <div
-      className="bg-gray-800 text-white absolute inset-0 flex flex-col items-center justify-center p-4"
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-    >
-      <div className="mb-4 text-2xl font-bold">Score: {score}</div>
-      <div className="mb-4 text-xl font-bold">High Score: {highScore}</div>
+    <>
       <button
         type="button"
         onClick={() => setShowHelp(true)}
-        className="absolute top-4 right-4 p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
+        className="absolute top-4 right-4 p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors z-1 text-white"
       >
         <HelpCircle className="w-8 h-8" />
       </button>
-      <div className="w-[90vw] h-[90vw] md:w-[600px] md:h-[600px] @container/main relative flex flex-col">
-        <div className="relative gap-[3cqw] bg-gray-700 p-[3cqw] rounded-lg grow">
-          {grid.map((tile, index) => {
-            const y = Math.floor(index / 4);
-            const x = index % 4;
 
-            return (
-              <motion.div
-                key={tile.id}
-                layout
-                {...(tile.value !== 0 && {
-                  initial: { scale: 0, opacity: 0 },
-                  animate: { scale: 1, opacity: 1 },
-                })}
-                transition={{ duration: 0.2 }}
-                className={clsx(
-                  'merriweather absolute w-[20cqw] h-[20cqw] rounded-lg flex items-center justify-center text-[6.5cqw] font-bold',
-                  { 'z-10': tile.value !== 0 },
-                  getTileClass(tile.value),
-                )}
-                style={{
-                  left: `calc(4cqw + 24cqw * ${x})`,
-                  top: `calc(4cqw + 24cqw * ${y})`,
-                }}
-              >
-                {tile.value !== 0 ? tile.value : ''}
-              </motion.div>
-            );
-          })}
+      <div
+        className="bg-gray-800 text-white absolute inset-0 flex flex-col items-center justify-center p-4"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className="mb-4 text-2xl font-bold">Score: {score}</div>
+        <div className="mb-4 text-xl font-bold">High Score: {highScore}</div>
+        <div className="w-[90vw] h-[90vw] md:w-[600px] md:h-[600px] @container/main relative flex flex-col">
+          <div className="relative gap-[3cqw] bg-gray-700 p-[3cqw] rounded-lg grow">
+            {grid.map((tile, index) => {
+              const y = Math.floor(index / 4);
+              const x = index % 4;
+
+              return (
+                <motion.div
+                  key={tile.id}
+                  layout
+                  {...(tile.value !== 0 && {
+                    initial: { scale: 0, opacity: 0 },
+                    animate: { scale: 1, opacity: 1 },
+                  })}
+                  transition={{ duration: 0.2 }}
+                  className={clsx(
+                    'merriweather absolute w-[20cqw] h-[20cqw] rounded-lg flex items-center justify-center text-[6.5cqw] font-bold',
+                    { 'z-10': tile.value !== 0 },
+                    getTileClass(tile.value),
+                  )}
+                  style={{
+                    left: `calc(4cqw + 24cqw * ${x})`,
+                    top: `calc(4cqw + 24cqw * ${y})`,
+                  }}
+                >
+                  {tile.value !== 0 ? tile.value : ''}
+                </motion.div>
+              );
+            })}
+          </div>
         </div>
-      </div>
 
-      {gameState === GameState.GameOver && (
-        <div className="merriweather absolute inset-0 bg-gray-800/90 backdrop-blur-xs flex flex-col items-center justify-center z-20">
-          <p className="text-4xl font-bold">Game Over!</p>
-          <p className="text-2xl font-bold">Score: {score}</p>
-          <p className="text-2xl font-bold">High Score: {highScore}</p>
-          <button
-            type="button"
-            onClick={restartGame}
-            className="mt-4 p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
-          >
-            <RotateCcw className="w-8 h-8" />
-          </button>
-        </div>
-      )}
-
-      {gameState === GameState.GameWon && (
-        <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex flex-col items-center justify-center">
-          <p className="text-4xl font-bold">You Win!</p>
-          <div className="flex mt-4 space-x-4">
-            <button
-              type="button"
-              onClick={continueGame}
-              className="p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
-            >
-              <Play className="w-8 h-8" />
-            </button>
+        {gameState === GameState.GameOver && (
+          <div className="merriweather absolute inset-0 bg-gray-800/90 backdrop-blur-xs flex flex-col items-center justify-center z-20">
+            <p className="text-4xl font-bold">Game Over!</p>
+            <p className="text-2xl font-bold">Score: {score}</p>
+            <p className="text-2xl font-bold">High Score: {highScore}</p>
             <button
               type="button"
               onClick={restartGame}
-              className="p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
+              className="mt-4 p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
             >
               <RotateCcw className="w-8 h-8" />
             </button>
           </div>
-        </div>
-      )}
+        )}
 
-      {showHelp && (
-        <div className="absolute inset-0 bg-gray-800/90 backdrop-blur-xs flex flex-col items-center justify-center z-30 p-4">
-          <div className="bg-gray-700 p-6 rounded-lg max-w-lg w-full relative">
-            <button
-              type="button"
-              onClick={() => setShowHelp(false)}
-              className="absolute top-2 right-2 p-3 rounded-full bg-gray-600 hover:bg-gray-500 transition-colors text-white"
-            >
-              <X />
-            </button>
-            <div
-              id="help-content-modal"
-              // biome-ignore lint/security/noDangerouslySetInnerHtml: it's ok
-              dangerouslySetInnerHTML={{
-                __html:
-                  document.getElementById('help-content')?.innerHTML || '',
-              }}
-            />
+        {gameState === GameState.GameWon && (
+          <div className="absolute inset-0 bg-gray-800 bg-opacity-75 flex flex-col items-center justify-center">
+            <p className="text-4xl font-bold">You Win!</p>
+            <div className="flex mt-4 space-x-4">
+              <button
+                type="button"
+                onClick={continueGame}
+                className="p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
+              >
+                <Play className="w-8 h-8" />
+              </button>
+              <button
+                type="button"
+                onClick={restartGame}
+                className="p-2 rounded-full bg-gray-700 hover:bg-gray-600 transition-colors"
+              >
+                <RotateCcw className="w-8 h-8" />
+              </button>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+
+        {showHelp && (
+          <div className="absolute inset-0 bg-gray-800/90 backdrop-blur-xs flex flex-col items-center justify-center z-30 p-4">
+            <div className="bg-gray-700 p-6 rounded-lg max-w-lg w-full relative">
+              <button
+                type="button"
+                onClick={() => setShowHelp(false)}
+                className="absolute top-2 right-2 p-3 rounded-full bg-gray-600 hover:bg-gray-500 transition-colors text-white"
+              >
+                <X />
+              </button>
+              <div
+                id="help-content-modal"
+                // biome-ignore lint/security/noDangerouslySetInnerHtml: it's ok
+                dangerouslySetInnerHTML={{
+                  __html:
+                    document.getElementById('help-content')?.innerHTML || '',
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
-
 
 export default App;
 
